cdd265f73e457b4297c28cb41be829063d2ebd0e
Aadesh Rajbhandari <cresthaadork124@gmail.com>
2024-07-22 20:07:38 +1000

    Delete a book with mongoose

--- backend/index.js
+++ backend/index.js
@@ -101,6 +101,23 @@ app.get('/books/:id', async (request,response) => {
          }
         });
 
+    //ROute to DELETE a book
+        app.delete('/books/:id', async (request,response) => {
+            try {
+                const { id } = request.params;
+
+                const result = await Book.findByIdAndDelete(id);
+                if(!result){
+                    return response.status(404).json({message: 'Book not found'});
+                }
+                return response.status(200).send({message: 'Book deleted successfully'});
+
+            } catch(error){
+                console.log(error.message);
+                response.status(500).send({message: error.message });
+            }
+        })
+
 
 mongoose
     .connect(mongoDBURL)
